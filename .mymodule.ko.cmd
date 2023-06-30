78da3cc94ec7f3f9cac735cae49a99ac4448e34a
Guilherme Chiarotto de Moraes <110139874+Guichiarotto@users.noreply.github.com>
2023-06-29 22:37:02 -0300

    Delete .mymodule.ko.cmd

--- .mymodule.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/gabriel/Trab-SO/mymodule.ko := ld -r  -m elf_x86_64 -z noexecstack   --build-id=sha1  -T scripts/module.lds -o /home/gabriel/Trab-SO/mymodule.ko /home/gabriel/Trab-SO/mymodule.o /home/gabriel/Trab-SO/mymodule.mod.o;  true
