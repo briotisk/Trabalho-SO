6144c6dcd13a378c898034c1f41b748f949086ca
Guilherme Chiarotto de Moraes <110139874+Guichiarotto@users.noreply.github.com>
2023-06-29 22:36:42 -0300

    Delete .Module.symvers.cmd

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/gabriel/Trab-SO/Module.symvers := sed 's/\.ko$$/\.o/' /home/gabriel/Trab-SO/modules.order | scripts/mod/modpost -m -a  -o /home/gabriel/Trab-SO/Module.symvers -e -i Module.symvers   -T -
